dc4683d37d4de3a39c78658ec1350c909c1eae00
Jaffar Hussein <jaffhussein@icloud.com>
2023-12-12 12:57:28 +0100

    Implement alignment scoring

--- sequences.c
+++ sequences.c
@@ -228,8 +228,7 @@ void parse_file(char *address, Sequence tab_sequences[])
     while (start != -1)
     {
         start = extract_next_sequence(code, start, &sequence);
-        tab_sequences[counter] = sequence;
-        counter++;
+        tab_sequences[counter++] = sequence;
     }
 }
 
