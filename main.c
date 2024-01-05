5fc9cb89303eca1c8c9ef2dcf31f4ef9011dfddf
Jaffar Hussein <jaffhussein@icloud.com>
2024-01-05 17:45:49 +0100

    Final CMD functionality

--- main.c
+++ main.c
@@ -60,58 +60,13 @@ int main(int argc, char *argv[])
     }
     char *file = "cat_dna.fasta";
     char *file_aligne = "cat_dna_aligne.fasta";
-    if (argc == 1)
-    {
-        printf("\033[31mError: No arguments provided.\033[0m\n");
-        printf("\033[32mUsage: ./phylo SEQUENCES ALIGNEMENT MATRICE_DISTANCE UPGMA NJ\033[0m\n");
-        return 1;
-    }
 
-    for (int i = 1; i < argc; i++)
-    {
-        for (int j = 0; j < strlen(argv[i]); j++)
-        {
-            argv[i][j] = toupper((unsigned char)argv[i][j]);
-        }
-
-        if (strcmp(argv[i], "SEQUENCES") == 0)
-        {
-            SEQUENCES = 1;
-        }
-        else if (strcmp(argv[i], "ALIGNEMENT") == 0)
-        {
-            ALIGNEMENT = 1;
-        }
-        else if (strcmp(argv[i], "MATRICE_DISTANCE") == 0)
-        {
-            MATRICE_DISTANCE = 1;
-        }
-        else if (strcmp(argv[i], "UPGMA") == 0)
-        {
-            UPGMA = 1;
-        }
-        else if (strcmp(argv[i], "NJ") == 0)
-        {
-            NJ = 1;
-        }
-        else
-        {
-            printf("\033[31mError: Invalid argument '%s'.\033[0m\n", argv[i]);
-            printf("\033[32mUsage: ./phylo SEQUENCES ALIGNEMENT MATRICE_DISTANCE UPGMA NJ\033[0m\n");
-            return 1;
-        }
-    }
-    char *file = "cat_dna.fasta";
-    char *file_aligne = "cat_dna_aligne.fasta";
-
-    if (SEQUENCES == 1)
-    {
     if (SEQUENCES == 1)
     {
         printf("----------------");
         printf("SEQUENCES");
         printf("----------------\n");
-        printf("Fichier: '%s'\n", file);
+        printf("Je vais parser le fichier : '%s'\n", file);
         show_sequences_file(file);
     }
 
@@ -120,14 +75,10 @@ int main(int argc, char *argv[])
     */
     if (ALIGNEMENT == 1)
     {
-    if (ALIGNEMENT == 1)
-    {
         printf("----------------");
         printf("ALIGNEMENT");
         printf("----------------\n");
 
-        char *ali1 = "-ACTCCTGA";
-        char *ali2 = "ATCTCGTGA";
         char *ali1 = "-ACTCCTGA";
         char *ali2 = "ATCTCGTGA";
         print_quality_alignement(ali1, ali2, score_alignement(ali1, ali2));
@@ -151,12 +102,10 @@ int main(int argc, char *argv[])
         set_sequence(&seq2, "Sequence 2", "ATCTCGTGA");
         needleman_wunsch(seq1, seq2, alignement1, alignement2);
 
-        printf("\nAlignement des sequences '%s' et '%s' en utilisant l'algorithme Needleman Wunsch:\n", seq1.seq, seq2.seq);
+        printf("On cherche à aligner '%s' et '%s' en utilisant l'algorithme Needleman Wunsch, on obtient comme alignement :\n", seq1.seq, seq2.seq);
         print_quality_alignement(alignement1, alignement2, score_alignement(alignement1, alignement2));
     }
 
-    if (MATRICE_DISTANCE == 1)
-    {
     if (MATRICE_DISTANCE == 1)
     {
         printf("----------------");
@@ -167,16 +116,12 @@ int main(int argc, char *argv[])
 
     if (UPGMA == 1)
     {
-    if (UPGMA == 1)
-    {
         printf("----------------");
         printf("UPGMA");
         printf("----------------\n");
         show_tree(file_aligne, 'U');
     }
 
-    if (NJ == 1)
-    {
     if (NJ == 1)
     {
         printf("----------------");
